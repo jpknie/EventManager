5790467f0f83d2c508e18be8435f3b37652b4bfb
Jani Nieminen <jpkniem@gmail.com>
2015-09-17 12:59:47 +0300

    Add route to attendance update action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  resources :events
+  resources :events do
+    collection do
+      post :update_user_attendance
+    end
+  end
+
 # authenticated :user do
 #   root 'entries#index', as: "authenticated_root"
 # end
