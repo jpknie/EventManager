9c9dcc4697028be99d3661f661303e2f16205982
Jani Nieminen <jpkniem@gmail.com>
2015-09-20 21:17:52 +0300

    Destroy instance from join table with dependent:destroy, when model is destroyed.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	has_many :attendances
-	has_many :users, through: :attendances
+	has_many :users, through: :attendances, dependent: :destroy
 	belongs_to :user
 	belongs_to :location
 end
