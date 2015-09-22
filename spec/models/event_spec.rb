48de257f451fbcb15bc479143bf13ca832ec6cc8
Jani Nieminen <jpkniem@gmail.com>
2015-09-22 09:11:48 +0300

    Destroy attendance test cases for event and user models.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 describe Event do
 	it "has a valid factory" do
-		FactoryGirl.create(:event).should be_valid
+		expect(FactoryGirl.create(:event)).to be_valid
 	end
-	it "destroys attendances when destroyed"
+	it "destroys attendances when destroyed" do
+		#1) Create user and event instances
+		event = FactoryGirl.create(:event)
+		user = FactoryGirl.create(:user)
+
+		#2) Create attendance instance and associate user and event with it
+		attendance = Attendance.new
+		attendance.user_id = event.id
+		attendance.event_id = user.id
+		attendance.save
+
+		#3) Destroy event instance, and assert that attendance is destroyed with it
+		event.destroy
+		assert Attendance.where(event_id: event.id).present? == false
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 describe User do
 	it "has a valid factory" do
-		FactoryGirl.create(:user).should be_valid
+		expect(FactoryGirl.create(:user)).to be_valid
 	end
-	it "destroys attendances when destroyed"
-	#	1) Create Attendance.new with user_id and event_id
-	#   2) Destroy event
-    #   3) Check that Attendance is removed
+	it "destroys attendances when destroyed" do
+		#1) Create user and event instances
+		event = FactoryGirl.create(:event)
+		user = FactoryGirl.create(:user)
+
+		#2) Create attendance instance and associate user and event with it
+		attendance = Attendance.new
+		attendance.user_id = event.id
+		attendance.event_id = user.id
+		attendance.save
+
+		#3) Destroy user instance, and assert that attendance is destroyed with it
+		user.destroy
+		assert Attendance.where(user_id: user.id).present? == false
+	end
 end
